f89d6b77f6eed012a2fc1e78631d7822d3c26acc
mcause <m.cause88@gmail.com>
2021-11-08 22:29:34 -0500

    initial commit

---
+++ variable-notes.js
@@ -0,0 +1,122 @@
+// let myFavoriteTvShow = 'Dexters Lab';
+// console.log('Kid:', myFavoriteTvShow);
+// myFavoriteTvShow = 'Reservation Dogs';
+// console.log('Adult:', myFavoriteTvShow);
+
+// //Math.truc returns the integer part of a number by removing fractional digits
+
+// let num1 =(37.53);      
+// console.log(num1);
+// console.log(Math.trunc(num1));// How I beleive we would code Math.trunc
+
+// console.log(Math.trunc(37.53));// Alternative way but same answer 
+
+// console.log(Math.trunc(0.7568));// even if the integer 
+
+// console.log(Math.trunc(-4.7342));//Negative integers remain negative just lose the fractional digits   
+
+// console.log(Math.trunc(176.987623)); 
+
+//Number Hw for monday 11/8/21
+// let num1 = 1080;
+// console.log(num1);
+// console.log(num1/2);
+// num1 = 1080;
+// console.log(num1);
+// let num2 = -38;
+// console.log(num2);
+// let num3= -94;
+// console.log(num3);
+// console.log(num2 - num3);
+// let num4=(14);
+// let num5=(6);
+// let num6=(5);
+// console.log(num4, num5, num6);
+// console.log(num4*num5*num6);
+// let num7=(24);
+// let num8=(4);
+// console.log(num7, num8);
+// console.log(num7/num8);
+// let num9=(73);
+// let num10=(5);
+// console.log(num9, num10);
+// console.log(num9%num10);
+// let minNum = Math.min(10,20,30,40,50);
+// console.log(minNum)
+// let maxNum = Math.max(-1,-2,-3,-4,-5);
+// console.log(maxNum);
+// let num11 = 63;
+// console.log(num11%2)
+// let sqRt144 = Math.sqrt(144);
+// console.log(sqRt144);
+// let pow = Math.pow(3, 7);
+// console.log(pow)
+// let round = Math.round(73.2);
+// console.log(round)
+// let floorHw= Math.floor(9.9);
+// console.log(floorHw)
+// let absHw = Math.abs(-1231);
+// console.log(absHw);
+
+
+// let noCaps = 'This Should Not Have Capital Letters!';
+// console.log(noCaps.toLowerCase());
+
+// let allCaps = "i'm very loud!";
+// console.log(allCaps.toUpperCase());
+
+// let duck =('Mallards');
+// let family =('my cousin'+'s');
+// let goose =('Geese');
+// console.log ('I am eager to go hunting for '.concat(duck, ' with ',family, ' and also ', goose ));
+
+// let stringOfNames =['Ally', 'Becca', 'Carlos', 'Drake', 'Edgar', 'Felicity', 'Greta', 'Hector']
+// console.log(stringOfNames.length)
+// console.log(stringOfNames);
+
+
+
+// let hi = "hello";
+// let bye = "goodbye";
+// let concatenatedGreeting = hi + bye;
+// console.log(concatenatedGreeting);
+
+// const hiFriend = "Hello Friend"
+// console.log(hiFriend.slice(6,14));
+
+// const word1 = "hello";
+// const word2 = "world";
+// const word3 = "car";
+// const word4 = "football";
+// const word5 = "soccer";
+// const word6 = "eat";
+// const word7 = "cookie";
+// const word8 = "favorite";
+// const word9 = "I";
+// const word10 = "to";
+// const word11 = "my";
+// const word12 = "is";
+// const word13 = "and";
+// const word14 = "like";
+// const savedTempLit = '${word1} '
+
+// null === undefined 
+// 5 == "5"
+// 10 !=="10"
+// 20 =='20'
+// 20>= "20"
+// 30 < 35
+
+let horseName = "Seabiscut";
+let position = 2;
+
+if(position === 1){
+    console.log(horseName,"finished in first place!");
+} else if(position === 2){
+     console.log(horseName,"finished in second place!");
+} else if (position === 3){
+    console.log(horseName,"finished in third place!");
+} else if (position >= 4){
+    console.log(horseName,"did not finish in the top 3.");
+}
+ 
