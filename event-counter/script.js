3936b2a3d9b002ac2437f0cc8a8fd13d40def612
mcause <m.cause88@gmail.com>
2021-12-11 12:06:09 -0500

    eventCounter

--- event-counter/script.js
+++ event-counter/script.js
@@ -1,19 +1,38 @@
 const body = document.querySelector("body");
+body.setAttribute('style', 'background-color: burlywood');
 
 const newMent = document.createElement('h1');
 body.appendChild(newMent);
 newMent.innerText = 'Hello I am the child of body';
+newMent.setAttribute('style', 'background-color : cyan')
+newMent.setAttribute.border = '4px double black';
+newMent.innerText = '0'
 
 const buttonMore = document.createElement('button');
-buttonMore.addEventListener('click', () => {
-    buttonMore = document.querySelector('.newMent').value;
-    buttonMore.innerText = 'Increment';
-    body.append(buttonMore);
-})
+body.appendChild(buttonMore);
+buttonMore.innerText = 'Increment';
+buttonMore.setAttribute('class', 'boxButton');
 
 const buttonLess = document.createElement('button');
+body.appendChild(buttonLess);
+buttonLess.innerText = 'Decrement';
+buttonLess.setAttribute('class', 'boxButton')
+
 buttonMore.addEventListener('click', () => {
-    buttonLess = document.querySelector('.newMent').value;
-    buttonLess.innerText = 'Decrement';
-    body.append(buttonLess);
+    newMent.innerText ++;
+    // newMent.innerText = parseInt(newMent.innerText) +1;
+})
+
+buttonLess.addEventListener('click', () => {
+    newMent.innerText --;
+    // newMent.innerText = parseInt(newMent.innerText) -1;
+})
+
+const deleteBut = document.createElement('button');
+body.appendChild(deleteBut);
+deleteBut.innerText = 'delete';
+deleteBut.setAttribute('class', 'boxButton')
+
+deleteBut.addEventListener('click', () => {
+    newMent.innerText = '0' 
 })
