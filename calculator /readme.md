9c98aae9a4235657076491ed13ff80f680630c42
mcause <m.cause88@gmail.com>
2021-12-20 18:03:56 -0500

    answered all but one codewars exercise for homework

---
+++ calculator /readme.md
@@ -0,0 +1,28 @@
+Calculator Project
+
+my rendition of a calculator 
+
+[ ] Create an html, javascript, styles 
+
+[ ] Make sure they are all connected 
+
+[ ] Create boxes that will accept my inputs 
+
+[ ] Place math functions and numbers within the created boxes 
+
+[ ] Write code that will alow the functions to work with the numbers being clicked on 
+
+## -HTML-
+
+[ ] Create a calculator in html 
+
+## -CSS-
+
+[ ] Stylize that calculator with css 
+
+## -Javascript- 
+
+[ ] create logic that allows the user to click on numbers and use the given functions 
+
+[ ] return a solution to the math equation 
+
---
+++ calculator /script.js
@@ -0,0 +1,22 @@
+// create the calculator boarder 
+// create buttons for different functions 
+// make the functions work once clicked on 
+// make the result of the function appear on the screen 
+// make a clear button that wont accept the previous inputs
+// make the calculator enjoyable but make it work !!!
+const calculator = document.querySelector('.calculator');
+
+const inputBox = document.createElement('div');
+inputBox.setAttribute('class', 'inputBox');
+inputBox.setAttribute('data', 1);
+calculator.appendChild(inputBox);
+
+const box1 = document.createElement('div');
+const box2 = document.createElement('div');
+const box3 = document.createElement('div');
+const box4 = document.createElement('div');
+const box5 = document.createElement('div');
+const box6 = document.createElement('div');
+const box7 = document.createElement('div');
+
+
